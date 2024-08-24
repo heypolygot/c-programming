0c8b551b30b8feb2d6ae2bc03401da5e9dbf321d
heypolygot <helloajaydey@gmail.com>
2024-08-24 19:44:34 +0530

    modifications

--- Guess_the_number.c
+++ Guess_the_number.c
@@ -3,24 +3,28 @@
 #include <stdlib.h>
 
 void main (void){
-	printf("Welcome...!\nThis is a game of guessing random numbers.\n/In which you will get 5 chances to guess the number.\nIf you guessed it right you will win either loose the game.\n");
+	printf("Welcome...!\nThis is a game of guessing random numbers.\nIn which you will get 5 chances to guess the number.\nIf you guessed it right you will win either loose the game.\n");
 	srand(time(NULL));
 	int n = rand() % 21;
 	int g, r = 1;
 	while (r <= 5){
-		printf("Guess a number between 0 - 20:\n");
+		printf("\n\nGuess a number between 0 - 20:\n");
 		scanf("%d", &g);
 		if (g == n){
 			printf("Congrats...You guessed the number!\n");
 			break;
 
-		}else if (g != n && r == 5){
-			printf("Oops..!! you guessed it wrong...!\n");
-			printf("You have %d chances left.\nYOU LOOSE...\nThe Number was %d\n", 5 - r, n);
+		}else if (g != n && g < n){
+			printf("Oops Wrong...The number is greater than that.\n");
+			printf("You have %d chances left.\n", 5 - r);
+		}else if (g != n && g < n){
+			printf("Oops Wrong...The number is smaller than that.\n");
+			printf("You have %d chances left.\n", 5 - r);
 		}else{
 			printf("Oops..!! you guessed it wrong...!\n");
 			printf("You have %d chances left.\n", 5 - r);
 		}
 		r++;
 	}
+	printf("Sorry You loose,\nThe number was %d.\n",n);
 }
